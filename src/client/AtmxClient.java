8bbe058f18ff3f339d6e612bc0aeae8881e5ac91
Jason Savlov <jsavlov@oswego.edu>
2016-05-09 14:11:35 -0400

    Added a custom host constructor to client class

--- src/client/AtmxClient.java
+++ src/client/AtmxClient.java
@@ -3,30 +3,47 @@ package client;
 import io.atomix.AtomixClient;
 import io.atomix.catalyst.transport.Address;
 import io.atomix.catalyst.transport.NettyTransport;
+import io.atomix.collections.internal.QueueCommands;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AtmxClient {
 
+    private List<Address> clusterHostList = null;
+    private static final List<Address> DEFAULT_HOSTS;
+
+    static {
+        DEFAULT_HOSTS = Arrays.asList(
+                new Address("127.0.0.1", 2703),
+                new Address("127.0.0.1", 2715),
+                new Address("127.0.0.1", 8700)
+        );
+    }
+
     public AtmxClient() {
+        // No hosts provided.. Use the predefined set
+        this.clusterHostList = new ArrayList<>(DEFAULT_HOSTS);
         init();
     }
 
-    public void init() {
+    public AtmxClient(List<Address> hostList)
+    {
+        this.clusterHostList = hostList;
+    }
+
+    private void init() {
 
         AtomixClient client = AtomixClient.builder()
                 .withTransport(new NettyTransport())
                 .build();
 
-        List<Address> cluster = Arrays.asList(
-                new Address("127.0.0.1", 2703),
-                new Address("127.0.0.1", 2715),
-                new Address("127.0.0.1", 8700)
-        );
-
-        client.connect(cluster).thenRun(() -> {
+        client.connect(clusterHostList).thenRun(() -> {
             System.out.println("Client connected!");
         });
+
+
 
     }
 
